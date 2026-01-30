96ba5c42189f9cf590a0834d7cd9a4091fd0961e
PanciWartegX <revandragd@gmail.com>
2026-01-30 17:50:01 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,15 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
+// Konfigurasi terbaru sesuai yang Anda lampirkan
 const firebaseConfig = {
   apiKey: "AIzaSyBaEkKJzyohsq6GK5GWygai0q3V0O_MkLY",
   authDomain: "web-foksi-9acb9.firebaseapp.com",
   projectId: "web-foksi-9acb9",
   storageBucket: "web-foksi-9acb9.firebasestorage.app",
   messagingSenderId: "811305636316",
-  appId: "1:811305636316:web:1f3c8641ed7df0a1203f19",
-  measurementId: "G-SHW9C8529Y"
+  appId: "1:811305636316:web:42718618e39e56ee203f19", // Menggunakan appId terbaru
+  measurementId: "G-BN0KBQQMBJ" // Menggunakan measurementId terbaru
 };
 
 // Initialize Firebase
@@ -20,4 +21,5 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Export agar bisa digunakan di auth.js dan database.js
 export { app, analytics, auth, db };
